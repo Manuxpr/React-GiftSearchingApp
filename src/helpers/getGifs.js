dc3291cdfe1f5794c1c18d93517ad4254e5907e7
manuxpr <manuelnb@iescastelar.com>
2024-05-13 11:33:47 +0200

    Docs created

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=7Iy4p029OQ0WHGueCzCWOZH2gJDWIDQt&q=${category}&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=7Iy4p029OQ0WHGueCzCWOZH2gJDWIDQt&q=${category}&limit=10y`;
     const resp = await fetch(url);
     const { data } = await resp.json();
     const gifs = data.map((img) => ({
